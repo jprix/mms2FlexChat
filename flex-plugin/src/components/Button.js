d9230e5a77df98cca74483cb113b951eb5963870
Terence Rogers <trogers@twilio.com>
2019-10-09 14:50:52 -0700

    Using material-ui buttons

--- flex-plugin/src/components/Button.js
+++ flex-plugin/src/components/Button.js
@@ -1,16 +1,37 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import { withStyles } from '@material-ui/core/styles';
 
-class Button extends React.Component {
+const styles = theme => ({
+    button: {
+        margin: 3,
+        'padding-left': 5,
+        'padding-right': 5,
+        'margin-bottom': 12,
+        'line-height': 1.25,
+        'text-transform': 'none'
+    },
+    input: {
+        display: 'none',
+    },
+});
+
+class MyButton extends React.Component {
     constructor(props) {
         super(props);
     }
     render() {
+        const { classes } = this.props;
         return (
-            <div>
-                <button style={{fontSize: '1rem'}} onClick={this.props.sendMedia}>{this.props.label}</button>
-            </div>
+            <Button
+                variant="contained"
+                className={classes.button}
+                onClick={this.props.sendMedia}
+            >
+                {this.props.label}
+            </Button>
         );
     }
 }
 
-export default Button;
+export default withStyles(styles)(MyButton);
